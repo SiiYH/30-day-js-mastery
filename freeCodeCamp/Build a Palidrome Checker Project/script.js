b4e0eea7520287bcbd016ce83426db2caea5524f
heyheysii <syewhei@gmail.com>
2025-08-05 07:46:35 +0800

    javascript certificate project

--- freeCodeCamp/Build a Palidrome Checker Project/script.js
+++ freeCodeCamp/Build a Palidrome Checker Project/script.js
@@ -1,12 +1,24 @@
-const button = document.getElementById("check-btn");
+function isPalidrome(str){
+  const cleaned = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  
+  return cleaned === cleaned.split('').reverse().join('');
+}
+
+const checkPalidrome = ()=>{
 const input = document.getElementById("text-input");
 const result = document.getElementById("result");
-button.addEventListener("click", () => {
-  const value = input.value.trim();
 
+const value = input.value.trim();
   if (value === ''){
     alert('Please input a value');
-  }else{
-    result.textContent = value + ' is a palindrome';
   }
-})
+  else if (isPalidrome(value)) {
+    result.textContent = `${value} is a palindrome`;
+  }else {
+    result.textContent = `${value} is not a palindrome`;
+  }
+}
+
+const button = document.getElementById("check-btn");
+
+button.addEventListener("click", checkPalidrome);
