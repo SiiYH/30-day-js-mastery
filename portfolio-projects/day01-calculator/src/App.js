ad53c1845af4e26cf3064a1aebb2c71a0a10a22c
heyheysii <manatee75@anglernook.com>
2025-08-04 23:20:13 +0800

    done calculator ui

---
+++ portfolio-projects/day01-calculator/src/App.js
@@ -0,0 +1,31 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="calculator-grid">
+      <div className = "output">
+        <div className = "previous-operand">99999000000999</div>
+        <div className = "current-operand">33335559999999999999999999999999</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+    </div>
+  );
+}
+export default App;
