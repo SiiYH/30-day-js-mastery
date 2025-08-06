9bb1f56f1f578c026cea9ea293a231afbe8b3be5
Sii33 <yewhei.sii@ibizcs.com>
2025-08-06 10:09:07 +0800

    solve gulp running on another pc

--- portfolio-projects/02-todo-list/gulpfile.js
+++ portfolio-projects/02-todo-list/gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
-const autoprefixer = require("gulp-autoprefixer");
+// const autoprefixer = require("gulp-autoprefixer");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
 const browserSync = require("browser-sync").create();
 
 const sassOptions = {
