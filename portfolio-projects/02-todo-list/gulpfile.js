dcfbb06dff67367120b4742c8dc2c03a98c6cd5b
Sii33 <yewhei.sii@ibizcs.com>
2025-08-06 10:10:29 +0800

    switch to more modern way of gulp autoprefixer

--- portfolio-projects/02-todo-list/gulpfile.js
+++ portfolio-projects/02-todo-list/gulpfile.js
@@ -1,9 +1,9 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
-// const autoprefixer = require("gulp-autoprefixer");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
+// const autoprefixer = require("gulp-autoprefixer"); //older
+const postcss = require("gulp-postcss");//modern way
+const autoprefixer = require("autoprefixer"); //modern way
 const browserSync = require("browser-sync").create();
 
 const sassOptions = {
