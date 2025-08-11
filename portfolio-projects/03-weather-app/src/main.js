f1d848ef0ac5cb37ab103aa9e377921ed0bed7c8
heyheysii <syewhei@gmail.com>
2025-08-11 14:59:00 +0800

    read the data passed inside

--- portfolio-projects/03-weather-app/src/main.js
+++ portfolio-projects/03-weather-app/src/main.js
@@ -1,1 +1,12 @@
 import "./style.css";
+
+import { getWeather } from "./weather";
+
+// getWeather(3.0439, 101.6201, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
+getWeather(3.0439, 101.6201, 'Asia/Singapore')
+.then(
+    data=>{
+        console.log(data)
+        // console.log(data.data)
+    }
+)
