fb70f5f997b624a19b456e442d16cfff1df94dac
Sii33 <heihei990205@gmail.com>
2025-09-03 17:39:14 +0800

    Day22: LeetCode Challenge

--- leetcode/Daily-solutions/226.InvertBinaryTree.js
+++ leetcode/Daily-solutions/226.InvertBinaryTree.js
@@ -25,3 +25,15 @@ var invertTree = function(root) {
 
     return root;
 };
+
+var invertTreeRecursive = function(root) {
+    if(root === null) return null;
+    
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    invertTree(root.left);
+    invertTree(root.right);
+    return root;
+};
