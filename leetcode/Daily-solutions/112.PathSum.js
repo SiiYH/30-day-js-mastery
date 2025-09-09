249a4cff9f153a6370793d02f60f56d3c7065333
heyheysii <syewhei@gmail.com>
2025-09-09 14:36:39 +0800

    Day 23: LeetCode Challenge-bfs

--- leetcode/Daily-solutions/112.PathSum.js
+++ leetcode/Daily-solutions/112.PathSum.js
@@ -13,3 +13,25 @@ var hasPathSumRecursive = function(root, targetSum) {
 
     return hasPathSumRecursive(root.left, remaining) || hasPathSum(root.right, remaining);
 }
+
+var hasPathSumBFS = function(root, targetSum) {
+
+    if(!root) return false;
+
+    let queue = [[root, root.val]];
+
+    while(queue.length > 0){
+        let [node, sum] = queue.shift();
+
+        if(!node.left && !node.right && targetSum === sum) return true;
+
+        if(node.left){
+            queue.push([node.left, sum + node.left.val]);
+        }
+        if(node.right){
+            queue.push([node.right, sum + node.right.val]);
+        }
+    }
+
+    return false;
+}
